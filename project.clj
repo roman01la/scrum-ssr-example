18ad1da2c31bf3fd449f5394de9b93e113dbd625
Roman Liutikov <roman01la@romanliutikov.com>
2017-10-04 23:03:03 +0300

    use prum

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.521"]
                  [org.clojure/core.match "0.3.0-alpha4"]
@@ -15,8 +15,9 @@
                  [com.cognitect/transit-clj "0.8.300"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [hiccup "1.0.5"]
-                 [rum "0.10.8"]
-                 [org.roman01la/scrum "2.3.0-SNAPSHOT"]
+                 [org.roman01la/prum "0.10.8"]
+                 [org.roman01la/scrum "2.3.0-SNAPSHOT"
+                  :exclusions [rum]]
                  [bidi "2.0.17"]
                  [kibu/pushy "0.3.7"]]
 
@@ -25,14 +26,16 @@
   :source-paths ["src"]
 
   :cljsbuild {:builds
-              [{:id "dev"
+              [{:id           "dev"
                 :source-paths ["src"]
-                :compiler {:main client.core
-                           :preloads [devtools.preload]
-                           :asset-path "/js/compiled/out"
-                           :output-to "resources/public/js/compiled/bundle.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true}}]}
+                :compiler     {:main                 client.core
+                               :preloads             [devtools.preload]
+                               :asset-path           "/js/compiled/out"
+                               :output-to            "resources/public/js/compiled/bundle.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true
+                               :install-deps         true
+                               :verbose              true}}]}
 
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component.repl "0.2.0"]
